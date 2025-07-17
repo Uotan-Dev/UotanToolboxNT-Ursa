6afd177dcc3dcd015b3c8d41c45dcad59f491253
赵紫菜 <25471437+13584452567@users.noreply.github.com>
2025-07-17 14:42:16 +0800

    Rename splash window files and update references

--- UotanToolboxNT_Ursa/App.axaml.cs
+++ UotanToolboxNT_Ursa/App.axaml.cs
@@ -20,12 +20,10 @@ public partial class App : Application
         Assets.Resources.Culture = new CultureInfo("zh-CN");
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MvvmSplashWindow()
             {
-                DataContext = new SplashViewModel()
+                DataContext = new SplashWindowViewModel()
             };
         }
 
