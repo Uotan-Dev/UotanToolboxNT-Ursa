a2d62af1fb172fa62b00c105fe495cec30c6e5d9
mujianwu <lrh15563910083@163.com>
2025-07-23 22:19:29 +0800

    初步完成首页UI

--- UotanToolboxNT_Ursa/Pages/Home.axaml.cs
+++ UotanToolboxNT_Ursa/Pages/Home.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System;
+using UotanToolboxNT_Ursa.Models;
 
 namespace UotanToolboxNT_Ursa.Pages;
 
@@ -7,5 +12,26 @@ public partial class Home : UserControl
     public Home()
     {
         InitializeComponent();
+    }
+
+    public async void CopyButton_OnClick(object sender, RoutedEventArgs args)
+    {
+        if (sender is Button button)
+        {
+            Avalonia.Input.Platform.IClipboard clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+            DataObject dataObject = new DataObject();
+            if (button.Content != null)
+            {
+                string text = button.Content.ToString();
+                if (text != null)
+                {
+                    dataObject.Set(DataFormats.Text, text);
+                }
+            }
+            if (clipboard != null)
+            {
+                await clipboard.SetDataObjectAsync(dataObject);
+            }
+        }
     }
 }
--- UotanToolboxNT_Ursa/ViewModels/HomeViewModel.cs
+++ UotanToolboxNT_Ursa/ViewModels/HomeViewModel.cs
@@ -1,12 +1,91 @@
+using Avalonia.Collections;
+using Avalonia.Controls.Notifications;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+using UotanToolboxNT_Ursa.Models;
 
 namespace UotanToolboxNT_Ursa.ViewModels;
 
 public partial class HomeViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _progressDisk = "0", _memLevel = "0", _status = "--", _bLStatus = "--",
+    _vABStatus = "--", _codeName = "--", _vNDKVersion = "--", _cPUCode = "--",
+    _powerOnTime = "--", _deviceBrand = "--", _deviceModel = "--", _systemSDK = "--",
+    _cPUABI = "--", _displayHW = "--", _density = "--", _boardID = "--", _platform = "--",
+    _compile = "--", _kernel = "--", _selectedSimpleContent = null, _diskType = "--",
+    _batteryLevel = "0", _batteryInfo = "--", _useMem = "--", _diskInfo = "--";
+    [ObservableProperty] private static AvaloniaList<string> _simpleContent;
 
     public HomeViewModel()
     {
 
+    }
+
+    [RelayCommand]
+    public async Task FreshDeviceList()
+    {
+        
+    }
+
+    [RelayCommand]
+    public async Task OpenAFDI()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task Open9008DI()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenUSBP()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenReUSBP()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task RebootSys()
+    {
+        
+    }
+
+    [RelayCommand]
+    public async Task RebootRec()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task RebootBL()
+    {
+        
+    }
+
+    [RelayCommand]
+    public async Task RebootFB()
+    {
+        
+    }
+
+    [RelayCommand]
+    public async Task PowerOff()
+    {
+        
+    }
+
+    [RelayCommand]
+    public async Task RebootEDL()
+    {
+        
     }
 }
