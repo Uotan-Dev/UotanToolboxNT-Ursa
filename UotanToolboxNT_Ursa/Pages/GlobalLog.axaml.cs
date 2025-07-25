52cc674e3e05dedb50bbf37cfb5ea342ba3a2c59
mujianwu <lrh15563910083@163.com>
2025-07-25 23:52:51 +0800

    调整UI

--- UotanToolboxNT_Ursa/Pages/GlobalLog.axaml.cs
+++ UotanToolboxNT_Ursa/Pages/GlobalLog.axaml.cs
@@ -1,9 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using System;
-using UotanToolboxNT_Ursa.Models;
 
 namespace UotanToolboxNT_Ursa.Pages;
 
@@ -12,26 +7,5 @@ public partial class GlobalLog : UserControl
     public GlobalLog()
     {
         InitializeComponent();
-    }
-
-    public async void CopyButton_OnClick(object sender, RoutedEventArgs args)
-    {
-        if (sender is Button button)
-        {
-            Avalonia.Input.Platform.IClipboard clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
-            DataObject dataObject = new DataObject();
-            if (button.Content != null)
-            {
-                string text = button.Content.ToString();
-                if (text != null)
-                {
-                    dataObject.Set(DataFormats.Text, text);
-                }
-            }
-            if (clipboard != null)
-            {
-                await clipboard.SetDataObjectAsync(dataObject);
-            }
-        }
     }
 }
