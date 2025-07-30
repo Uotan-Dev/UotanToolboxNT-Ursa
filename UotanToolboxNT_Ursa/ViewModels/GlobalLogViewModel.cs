50c5bc882e74359878c631e5bf90b43da92dd4f8
mujianwu <lrh15563910083@163.com>
2025-07-30 10:16:28 +0800

    卡片

--- UotanToolboxNT_Ursa/ViewModels/GlobalLogViewModel.cs
+++ UotanToolboxNT_Ursa/ViewModels/GlobalLogViewModel.cs
@@ -5,6 +5,9 @@ namespace UotanToolboxNT_Ursa.ViewModels;
 public partial class GlobalLogViewModel : ObservableObject
 {
     [ObservableProperty] public static string _logContent = "";
+    [ObservableProperty] public static string _logContent1 = "";
+    [ObservableProperty] public static string _logContent2 = "";
+    [ObservableProperty] public static string _logContent3 = "";
     public GlobalLogViewModel()
     {
 
