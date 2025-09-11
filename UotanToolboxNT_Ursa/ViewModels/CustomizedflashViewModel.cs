e1a66ae34e3e2391cbf931172ebc3ac991b7518e
mujianwu <lrh15563910083@163.com>
2025-09-11 20:57:35 +0800

    完成自定义刷入页面绑定

--- UotanToolboxNT_Ursa/ViewModels/CustomizedflashViewModel.cs
+++ UotanToolboxNT_Ursa/ViewModels/CustomizedflashViewModel.cs
@@ -1,10 +1,136 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
 namespace UotanToolboxNT_Ursa.ViewModels;
 
 public partial class CustomizedflashViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _systemFile = string.Empty, _productFile = string.Empty, _venderFile = string.Empty, _bootFile = string.Empty, _systemextFile = string.Empty,
+        _odmFile = string.Empty, _venderbootFile = string.Empty, _initbootFile = string.Empty, _imageFile = string.Empty, _part = string.Empty, _customizedflashLog = string.Empty;
+
     public CustomizedflashViewModel()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenSystemFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashSystemFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenProductFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashProductFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenVenderFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashVenderFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenBootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashBootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenSystemextFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashSystemextFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenOdmFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashOdmFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenVenderbootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashVenderbootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenInitbootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashInitbootFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task OpenImageFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task FlashImageFileBut()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task DisableVbmeta()
+    {
+
+    }
+
+    [RelayCommand]
+    public async Task SetOther()
     {
 
     }
