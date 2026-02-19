596e5d4785ca9c67fcec55adba491de970acac55
暮间雾 <lrh15563910083@163.com>
2026-02-20 01:01:15 +0800

    尝试修复左侧菜单

--- UotanToolboxNT_Ursa/Converters/ViewLocator.cs
+++ UotanToolboxNT_Ursa/Converters/ViewLocator.cs
@@ -1,22 +1,61 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+using UotanToolboxNT_Ursa.Pages;
+using UotanToolboxNT_Ursa.ViewModels;
 
 namespace UotanToolboxNT_Ursa.Converters;
 
 public class ViewLocator : IDataTemplate
 {
+    bool _isfirst = false;
     public Control? Build(object? param)
     {
-        if (param is null)
-        {
-            return null;
-        }
-
+        if (param is null) return null;
         var name = param.GetType().Name.Replace("ViewModel", "");
-        var type = Type.GetType("UotanToolboxNT_Ursa.Pages." + name);
-        return type != null ? (Control)Activator.CreateInstance(type)! : new TextBlock { Text = "Not Found: " + name };
+        try
+        {
+            var type = Type.GetType("UotanToolboxNT_Ursa.Pages." + name);
+            if (type != null)
+            {
+                return Activator.CreateInstance(type) as Control;
+            }
+
+            return GetViewByVM(param) ?? new TextBlock { Text = "Not Found : " + name };
+        }
+        catch (Exception e)
+        {
+            var reslut = _isfirst
+                ? new TextBlock
+                {
+                    Text = $"Activator.CreateInstance failure ::{e}",
+                    TextWrapping = Avalonia.Media.TextWrapping.Wrap
+                }
+                : GetViewByVM(param) ?? new TextBlock { Text = "Not Found : " + name };
+            _isfirst = false;
+            return reslut;
+        }
     }
 
-    public bool Match(object? data) => true;
+    public bool Match(object? data)
+    {
+        return true;
+    }
+    private Control? GetViewByVM(object vm)
+    {
+        return vm switch
+        {
+            AppmgrViewModel => new Appmgr(),
+            BasicflashViewModel => new Basicflash(),
+            CustomizedflashViewModel => new Customizedflash(),
+            FormatExtractViewModel => new FormatExtract(),
+            GlobalLogViewModel => new GlobalLog(),
+            HomeViewModel => new Home(),
+            ModifypartitionViewModel => new Modifypartition(),
+            ScrcpyViewModel => new Scrcpy(),
+            SettingsViewModel => new Settings(),
+            WiredflashViewModel => new Wiredflash(),
+            _ => null
+        };
+    }    
 }
