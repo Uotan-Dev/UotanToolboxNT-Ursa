cc1372c5e28033429e23e2756fa82df6c56cf1d3
暮间雾 <lrh15563910083@163.com>
2026-02-20 23:05:25 +0800

    恢复部分功能

--- UotanToolboxNT_Ursa/Views/MainWindow.axaml.cs
+++ UotanToolboxNT_Ursa/Views/MainWindow.axaml.cs
@@ -11,36 +11,36 @@ public partial class MainWindow : UrsaWindow
     public MainWindow()
     {
         InitializeComponent();
-        //_ = this.GetObservable(ClientSizeProperty).Subscribe(OnClientSizeChanged);
+        _ = this.GetObservable(ClientSizeProperty).Subscribe(OnClientSizeChanged);
     }
 
-    //private const double AspectRatio = 1197.0 / 825.0; // 设定目标宽高比（如16:9）
-    //private Size _lastSize = new(1197, 825);
+    private const double AspectRatio = 1197.0 / 825.0; // 设定目标宽高比（如16:9）
+    private Size _lastSize = new(1197, 825);
 
-    //private void OnClientSizeChanged(Size newSize)
-    //{
-    //    var deltaWidth = Math.Abs(newSize.Width - _lastSize.Width);
-    //    var deltaHeight = Math.Abs(newSize.Height - _lastSize.Height);
+    private void OnClientSizeChanged(Size newSize)
+    {
+        var deltaWidth = Math.Abs(newSize.Width - _lastSize.Width);
+        var deltaHeight = Math.Abs(newSize.Height - _lastSize.Height);
 
-    //    if (deltaWidth > deltaHeight)
-    //    {
-    //        用户主要在拖动宽度
-    //       var expectedHeight = newSize.Width / AspectRatio;
-    //        if (Math.Abs(newSize.Height - expectedHeight) > 1)
-    //        {
-    //            Height = expectedHeight;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        用户主要在拖动高度
-    //       var expectedWidth = newSize.Height * AspectRatio;
-    //        if (Math.Abs(newSize.Width - expectedWidth) > 1)
-    //        {
-    //            Width = expectedWidth;
-    //        }
-    //    }
+        if (deltaWidth > deltaHeight)
+        {
+            //用户主要在拖动宽度
+           var expectedHeight = newSize.Width / AspectRatio;
+            if (Math.Abs(newSize.Height - expectedHeight) > 1)
+            {
+                Height = expectedHeight;
+            }
+        }
+        else
+        {
+            //用户主要在拖动高度
+           var expectedWidth = newSize.Height * AspectRatio;
+            if (Math.Abs(newSize.Width - expectedWidth) > 1)
+            {
+                Width = expectedWidth;
+            }
+        }
 
-    //    _lastSize = new Size(Width, Height);
-    //}
+        _lastSize = new Size(Width, Height);
+    }
 }
